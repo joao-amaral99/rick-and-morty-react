1f004d51c9501dde7a73e8136d6a0b9a32c7d35a
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-04 13:46:27 -0300

    style: change style of input

--- src/components/SearchBar/styles.js
+++ src/components/SearchBar/styles.js
@@ -1,31 +1,24 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Form = styled.form`
-  display: flex;
-  justify-content: center;
-  margin-bottom: 3rem;
-  margin-top: 1rem;
+export const Input = styled.input`
+  background-color: #eaecef;
+  border: 2px solid #d6deeb;
+  border-radius: 5px;
+  font-size: 1.1rem;
+  height: 2.5rem;
+  letter-spacing: 0.6px;
+  margin: 3rem auto;
+  padding: 2px 8px;
+  width: 600px;
 
-  input {
-    background-color: #eaecef;
-    border: 2px solid #d6deeb;
-    border-radius: 5px;
-    font-size: 1.1rem;
-    height: 2.5rem;
-    letter-spacing: 0.6px;
-    padding: 2px 8px;
-    width: 400px;
-    
-
-    &:focus {
-      background-color: #fff;
-      box-shadow: 0 0 0 3px rgba(4, 173, 209, 0.8);
-      outline: none;
-      transition: 0.2s;
-    }
-
-    @media (max-width: 480px) {
-    width: 300px;
+  &:focus {
+    background-color: #fff;
+    box-shadow: 0 0 0 3px rgba(4, 173, 209, 0.8);
+    outline: none;
+    transition: 0.2s;
   }
+
+  @media (max-width: 480px) {
+    width: 300px;
   }
 `;
