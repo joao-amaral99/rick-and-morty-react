2566b0679800f31f9667f31d50e37abf9583bf74
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-04 13:48:00 -0300

    refactor: change prop name and add a useEffect to fetch data

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
-import { Container, Image, WrapButton, Btn, BtnDarkMode } from './styles';
-import { BsSun, BsFillMoonStarsFill } from 'react-icons/bs';
+import { useState, useEffect } from "react";
+import { Container, Image, WrapButton, Btn, BtnDarkMode } from "./styles";
+import { BsSun, BsFillMoonStarsFill } from "react-icons/bs";
 
-import Character from '../../components/Character'; 
-import SearchBar from '../../components/SearchBar';
-import Logo from '../../assets/rickandmorty.png';
-
+import Character from "../../components/Character";
+import SearchBar from "../../components/SearchBar";
+import Logo from "../../assets/rickandmorty.png";
 
 export default function Home() {
-  const [ characters, setCharacters ] = useState([]);
-  const [ darkMode, setDarkMode ] = useState(false);
-  const [ page, setPage ] = useState({});
+  const [characters, setCharacters] = useState([]);
+  const [darkMode, setDarkMode] = useState(false);
+  const [text, setText] = useState("");
+  const [page, setPage] = useState({});
 
   const apiURL = `https://rickandmortyapi.com/api/character/`;
 
@@ -20,71 +20,81 @@ export default function Home() {
 
       setCharacters(data.results);
       setPage(data.info);
-    } 
+    };
 
     fetchCharacters();
   }, []);
 
+  useEffect(() => {
+    const fetchCharacterByName = async (text) => {
+      const data = await fetchData(`${apiURL}?name=${text}`);
+
+      if (!data.results) {
+        return null;
+      }
+      setCharacters(data.results);
+    };
+
+    fetchCharacterByName(text);
+  }, [text]);
+
   const fetchData = async (url) => {
     const response = await fetch(url);
     return response.json();
-  }
+  };
 
   const searchCharactersByPage = async (url) => {
     const data = await fetchData(url);
 
     setCharacters(data.results);
     setPage(data.info);
-  }
+  };
 
   const handleWithDarkMode = () => {
     const body = document.body;
-    darkMode ? body.classList.remove('dark-mode') : body.classList.add('dark-mode');
-    
+    darkMode
+      ? body.classList.remove("dark-mode")
+      : body.classList.add("dark-mode");
+
     setDarkMode(!darkMode);
-  }
-
-  const onTermSubmit = async (term) => {
-    const data = await fetchData(`${apiURL}?name=${term}`);
-
-    setCharacters(data.results);
-    setPage(data.info);
-  }
+  };
 
   return (
     <>
       <Image src={Logo} alt="Logo image" width="600" height="360" />
-      <BtnDarkMode 
+      <BtnDarkMode
         onClick={handleWithDarkMode}
         darkMode={darkMode}
-        light={'#e5e5e5'}
-        dark={'#272727'}
+        light={"#e5e5e5"}
+        dark={"#272727"}
       >
-        {
-          darkMode ? <BsSun /> : <BsFillMoonStarsFill />
-        }
+        {darkMode ? <BsSun /> : <BsFillMoonStarsFill />}
       </BtnDarkMode>
 
-      <SearchBar onTermSubmit={onTermSubmit} />
+      <SearchBar value={text} onChange={(search) => setText(search)} />
 
       <Container>
-        { characters.map((data) => (   
-          <Character key={data.id} data={data}/>
-        ))}  
+        {characters.map((data) => (
+          <Character key={data.id} data={data} />
+        ))}
       </Container>
 
       <WrapButton>
         {page.prev ? (
-          <Btn onClick={() => searchCharactersByPage(page.prev)}>&larr; Prev</Btn>
-         ) : (
+          <Btn onClick={() => searchCharactersByPage(page.prev)}>
+            &larr; Prev
+          </Btn>
+        ) : (
           <Btn disabled>&larr; Prev</Btn>
-        )} 
+        )}
         {page.next ? (
-          <Btn onClick={() => searchCharactersByPage(page.next)}>Next &rarr;</Btn>
+          <Btn onClick={() => searchCharactersByPage(page.next)}>
+            Next &rarr;
+          </Btn>
         ) : (
           <Btn disabled>Next &rarr;</Btn>
         )}
       </WrapButton>
     </>
   );
-};
+}
