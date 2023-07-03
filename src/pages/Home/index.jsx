fee1f894d4d2949141a1b21cf5531a1dcff68f5d
Joao Amaral <joao.amaral.almeida99@gmail.com>
2023-07-02 21:52:37 -0300

    remove unused import

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,72 +1,71 @@
-import { useEffect, useState } from 'react';
-import { BsFillMoonStarsFill, BsSun } from 'react-icons/bs';
-import 'react-loading-skeleton/dist/skeleton.css';
-import { useQuery } from 'react-query';
-import { BtnDarkMode, Container, Image } from './styles';
+import { useEffect, useState } from "react";
+import { BsFillMoonStarsFill, BsSun } from "react-icons/bs";
+import { useQuery } from "react-query";
+import { BtnDarkMode, Container, Image } from "./styles";
 
-import Logo from '../../assets/rickandmorty.png';
-import Character from '../../components/Character';
-import SearchBar from '../../components/SearchBar';
+import Logo from "../../assets/rickandmorty.png";
+import Character from "../../components/Character";
+import SearchBar from "../../components/SearchBar";
 const apiURL = `https://rickandmortyapi.com/api/character/`;
 
 const fetchData = async (apiUrl) => {
-	const response = await fetch(apiUrl);
-	return response.json();
+  const response = await fetch(apiUrl);
+  return response.json();
 };
 
 export default function Home() {
-	const [characters, setCharacters] = useState([]);
-	const [darkMode, setDarkMode] = useState(false);
-	const [text, setText] = useState('');
+  const [characters, setCharacters] = useState([]);
+  const [darkMode, setDarkMode] = useState(false);
+  const [text, setText] = useState("");
 
-	useEffect(() => {
-		const fetchCharacterByName = async (text) => {
-			const data = await fetchData(`${apiURL}?name=${text}`);
+  useEffect(() => {
+    const fetchCharacterByName = async (text) => {
+      const data = await fetchData(`${apiURL}?name=${text}`);
 
-			if (!data.results) {
-				return null;
-			}
+      if (!data.results) {
+        return null;
+      }
 
-			setCharacters(data.results);
-		};
+      setCharacters(data.results);
+    };
 
-		fetchCharacterByName(text);
-	}, [text]);
+    fetchCharacterByName(text);
+  }, [text]);
 
-	const { data } = useQuery('characters', () => fetchData(apiURL), {
-		onSuccess: (data) => {
-			setCharacters(data.results);
-		},
-	});
+  const { data } = useQuery("characters", () => fetchData(apiURL), {
+    onSuccess: (data) => {
+      setCharacters(data.results);
+    },
+  });
 
-	const handleWithDarkMode = () => {
-		const body = document.body;
-		darkMode
-			? body.classList.remove('dark-mode')
-			: body.classList.add('dark-mode');
+  const handleWithDarkMode = () => {
+    const body = document.body;
+    darkMode
+      ? body.classList.remove("dark-mode")
+      : body.classList.add("dark-mode");
 
-		setDarkMode(!darkMode);
-	};
+    setDarkMode(!darkMode);
+  };
 
-	return (
-		<>
-			<Image src={Logo} alt="Logo Rick and Morty" width="600" height="360" />
-			<BtnDarkMode
-				onClick={handleWithDarkMode}
-				darkMode={darkMode}
-				light={'#e5e5e5'}
-				dark={'#272727'}
-			>
-				{darkMode ? <BsSun /> : <BsFillMoonStarsFill />}
-			</BtnDarkMode>
+  return (
+    <>
+      <Image src={Logo} alt="Logo Rick and Morty" width="600" height="360" />
+      <BtnDarkMode
+        onClick={handleWithDarkMode}
+        darkMode={darkMode}
+        light={"#e5e5e5"}
+        dark={"#272727"}
+      >
+        {darkMode ? <BsSun /> : <BsFillMoonStarsFill />}
+      </BtnDarkMode>
 
-			<SearchBar value={text} onChange={(search) => setText(search)} />
+      <SearchBar value={text} onChange={(search) => setText(search)} />
 
-			<Container>
-				{characters.map((character) => (
-					<Character key={character.id} data={character} />
-				))}
-			</Container>
-		</>
-	);
+      <Container>
+        {characters.map((character) => (
+          <Character key={character.id} data={character} />
+        ))}
+      </Container>
+    </>
+  );
 }
